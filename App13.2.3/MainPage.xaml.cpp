a87cd70ec0a53be3eb22dd0af315dbd2ce5c7d51
Kazu-SK <sonodans109@gmail.com>
2025-03-20 21:03:23 +0900

    プロジェクト ファイルを追加します。

---
+++ App13.2.3/MainPage.xaml.cpp
@@ -0,0 +1,40 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage クラスの実装。
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace App13_2_3;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void App13_2_3::MainPage::Button1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//メッセージダイアログを表示
+	Windows::UI::Popups::MessageDialog^ md = ref new Windows::UI::Popups::MessageDialog(
+		L"ユニバーサルアプリの世界へようこそ!"
+	);
+
+	//ユーザが応答するまで待機する
+	md->ShowAsync();
+
+}
